a2878e5cdb8b8c423669d42d509d83dd51be384e
HXYP-qwqwqwwei <qwqwqwwei@qq.com>
2021-01-19 01:56:49 +0800

    added some items

--- main/java/theskyisland/tsi/util/Reference.java
+++
@@ -1,9 +0,0 @@
-package theskyisland.tsi.util;
-
-public class Reference {
-    public static final String MODID = "theskyisland";
-    public static final String NAME = "The Island";
-    public static final String VERSION = "0.0.0";
-    public static final String CLIENT_PROXY_CLASS = "theislandmod.til.Proxy.ClientProxy";
-    public static final String SERVER_PROXY_CLASS = "theislandmod.til.Proxy.CommonProxy";
-}
